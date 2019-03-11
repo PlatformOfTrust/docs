de1723472be69b8f162710e66adccfa6e54e561e
kyyberi <jarkko@vimo.fi>
2019-03-11 22:47:54 +0200

    Added error handling

--- raml2markdown/build.py
+++ raml2markdown/build.py
@@ -4,7 +4,8 @@ import os
 from pathlib import Path
 import subprocess
 
-# Array of APIs. Call API_Raml_to_Slate(apiname) for each
+# Array of APIs. Call API_Raml_to_Slate(apiname) for each. Expected: API RAML file in folder which is the api name.
+# Inside that folder must be .raml file with the same name. Example: ./src/product-api/product-api.raml
 APIs = ["broker-api", "calendar-api", "product-api"]
 
 
@@ -19,24 +20,26 @@ def API_Raml_to_Slate(apiname):
   if jsonfile.exists():
     os.remove(jsonfile)
   else:
-    print("File: "+apiname+" delete failed. JSON formatted file not found!")
+    print("File: "+apiname+".json delete failed. JSON formatted file not found!")
 
   if slatefile.exists():
     os.remove(slatefile)
   else:
-    print("File: "+apiname+" delete failed. MD formatted file not found!")
+    print("File: "+apiname+".md delete failed. MD formatted file not found!")
 
-  # Generate API docs
-
-  # First convert RAML -> OpenAPISpec file
+  # Generate API docs. First convert RAML -> OpenAPISpec file
   jsoncmd = "./oas-raml-converter/lib/bin/converter.js --from RAML --to OAS20 ./src/" +apiname+ "/" +apiname+ ".raml > ./OAS/"+apiname+".json"
-  os.system(jsoncmd)
-
+  failure = os.system(jsoncmd)
+  if failure:
+    print("RAML -> OpenAPISpec failed. Trying next in array.")
+  else:
   # Convert from OpenAPISpec to Slate md
-  slatecmd= "swagger-to-slate -i ./OAS/" +apiname+ ".json -o ./slate/" +apiname+ ".md"
-  os.system(slatecmd)
+    slatecmd= "swagger-to-slate -i ./OAS/" +apiname+ ".json -o ./slate/" +apiname+ ".md"
+    failure = os.system(slatecmd)
+    if failure:
+      print("RAML -> Slate formatted md file creation failed. Trying next in array.")
 
-  #os.system('swagger-to-slate -i ./OAS/"+apiname+".json -o ./slate/+"apiname"+.md')
+
 
 for api in APIs:
   API_Raml_to_Slate(api)
