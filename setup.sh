e1df4b250ad6888bc4920c5e9b8145e2b5265449
Janne Enberg <janne.enberg@lietu.net>
2019-03-18 15:02:54 +0200

    More fixes

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ cd raml2markdown
 rm -rf ./oas-raml-converter
 git clone https://github.com/mulesoft/oas-raml-converter.git
 cd oas-raml-converter
+
+# Fixes running node via shebang on Linux
+sed -Ei 's@env node --harmony@env node@' lib/bin/*.js
+
 npm install
 npm run build
 cd ..
