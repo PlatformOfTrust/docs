a13354eca033114f7950fa8cad2c93c0c010b5b6
kyyberi <jarkko@vimo.fi>
2019-06-07 08:40:51 +0300

    styled

--- raml2markdown/build.py
+++ raml2markdown/build.py
@@ -102,10 +102,11 @@ def concatenate_files():
           if example_file_path.exists():
             with open(example_file_path) as sfile:
               print("Found example file: " + str(example_file_path) +" derived from line:" + copyline)
-              ofile.write("> <div class='hexagon-so'><div class='hexagon-inside-so'><div class='hexagon-inside2-so'>")
-              ofile.write("<a href='https://stackoverflow.com/questions/ask?guided=false&tags=platform-of-trusT' title='Get help from Stack Overflow' target='new'>Get help from Stack Overflow")
+              ofile.write("> <div class='hexagon-so'><div class='hexagon-inside-so'><div class='hexagon-inside2-so'><p>Do you need help in using the API? </p>")
+              ofile.write("<li><a href='https://stackoverflow.com/questions/tagged/platform-of-trust' title='Check out Platform of Trust questions and answers in Stack Overflow' target='new'>Checkout existing questions & answers</a></li>")
+              ofile.write("<li><a href='https://stackoverflow.com/questions/ask?guided=false&tags=platform-of-trusT' title='Ask a question in Stack Overflow' target='new'>Ask a question in Stack Overflow</a></li>")
               # ofile.write("<img src='images/raml.png' class='ramlSpec-lg'>")
-              ofile.write("</a></div></div></div>")
+              ofile.write("</div></div></div>")
               ofile.write("\n\n")
               ofile.write(example_desc)
               ofile.write(sfile.read()+"\n\n")
