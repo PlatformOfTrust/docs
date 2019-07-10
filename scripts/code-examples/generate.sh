346a8137ab1c11f6bcd42193fdc2171cef44ddef
Sven Laater <sven.laater@gmail.com>
2019-07-11 02:45:50 +0300

    ci: log generated files

--- scripts/code-examples/generate.sh
+++ scripts/code-examples/generate.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-
-# Download code example generator
+# Download
 curl -L -o raml2http.jar https://github.com/PlatformOfTrust/code-examples-generator/releases/download/latest/raml2http.jar > /dev/null
+
+# Show version
+java -jar raml2http.jar --version
 
 # Run
 # TODO make it fail in case of 'unknown options' etc.
-java -jar raml2http.jar -s ./raml2markdown/src -d ./code-examples -S http -H mockbin.org/request
+java -jar raml2http.jar \
+     -s ./raml2markdown/src \
+     -d $CODE_EXAMPLES \
+     -S http \
+     -H mockbin.org/request
+
+# Display generated files
+find $CODE_EXAMPLES -type f
