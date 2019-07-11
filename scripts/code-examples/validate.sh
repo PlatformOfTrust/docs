2796bf6e29c38856f6c9f6f518cba3f7b10f68a8
fbjorn <kopfabschneider@yandex.ru>
2019-07-11 23:36:38 +0300

    Fix poetry resolving

--- scripts/code-examples/validate.sh
+++ scripts/code-examples/validate.sh
@@ -5,6 +5,7 @@ set -exuo pipefail
 # install the tool
 git clone https://github.com/PlatformOfTrust/code-examples-validator
 cd code-examples-validator
+alias poetry=~/.poetry/bin/poetry
 poetry install --no-dev
 
 # run validation
