7531f1253592f55863da5ab3099a517a3484847e
fbjorn <kopfabschneider@yandex.ru>
2019-07-12 00:00:40 +0300

    Playing around with poetry

--- scripts/code-examples/validate.sh
+++ scripts/code-examples/validate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
+# make poetry available
+export PATH=${PATH}:${HOME}/.poetry/bin
 
 # install the tool
 git clone https://github.com/PlatformOfTrust/code-examples-validator
