85d5a8ab596dbefa2ac9df16a8aee3601993f28b
Sven Laater <sven.laater@gmail.com>
2019-07-17 16:28:05 +0300

    ci: fix typo

--- scripts/code-examples/api-check.sh
+++ scripts/code-examples/api-check.sh
@@ -10,7 +10,7 @@ STATUS_CODE=`curl -s -m 10 -o /dev/null -w "%{http_code}\n" ${SCHEME}://${HOST}/
 EXPECTED_CODE=200
 
 if [ $STATUS_CODE -eq $EXPECTED_CODE ]; then
-  echo "API response check passed ($STATUS_CODE})";
+  echo "API response check passed ($STATUS_CODE)";
 else
   echo "API response check failed.";
   echo "expected: " $EXPECTED_CODE;
