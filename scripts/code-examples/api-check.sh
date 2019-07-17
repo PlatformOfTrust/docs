3f720308fbec1945e4b7ef9e166b61eb9804d150
Sven Laater <sven.laater@gmail.com>
2019-07-17 16:17:26 +0300

    ci: fix API response check

--- scripts/code-examples/api-check.sh
+++ scripts/code-examples/api-check.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-set -xu
+set -u
 
-STATUS_CODE=`curl -s -m 10 -o /dev/null -w "%{http_code}\n" ${SCHEME}://${HOST}`
-EXPECTED_CODE=200
+# Use an endpoint that does not require authentication
+URL="${SCHEME}://${HOST}/products/v1"
+echo "Executing API response check: $URL"
+
+# Use an endpoint that does not require
+STATUS_CODE=`curl -s -m 10 -o /dev/null -w "%{http_code}\n" ${SCHEME}://${HOST}/products/v1`
 
 if [ $STATUS_CODE -eq $EXPECTED_CODE ]; then
   echo "API response check passed ($STATUS_CODE})";
 else
-  echo "API response check failed (${SCHEME}://${HOST})";
+  echo "API response check failed.";
   echo "expected: " $EXPECTED_CODE;
   echo "actual: " $STATUS_CODE;
-  travis_terminate 1;
+  exit 1;
 fi
